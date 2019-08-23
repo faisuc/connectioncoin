862085fb8873417cf70b7e792147b6ed2ad6e342
Faisan <necafasu@gmail.com>
2019-08-23 18:07:19 +0800

    Updates

--- resources/views/users/partials/sidebar.blade.php
+++ resources/views/users/partials/sidebar.blade.php
@@ -6,7 +6,7 @@
     <a href="{{ route('users.coins.index', ['user' => $user]) }}" class="list-group-item list-group-item-action {{ \Route::current()->getName() == 'users.coins.index' ? 'active' : '' }}">Coins</a>
     <a href="{{ route('users.stories.index', ['user' => $user]) }}" class="list-group-item list-group-item-action {{ \Route::current()->getName() == 'users.stories.index' ? 'active' : '' }}">Stories</a>
 </div> --}}
-<div class="card bg-dark text-white">
+<div class="card bg-dark text-white" style="border-radius: 2%;">
     <div class="opacity-me" style="
         position: absolute;
         bottom: 0;
@@ -15,24 +15,25 @@
         opacity: 0.3;
         background: #ffffff;
     "></div>
-    <img class="card-img" src="{{ $user->thePhoto }}" alt="Card image">
+    <img class="card-img" src="{{ $user->thePhoto }}" alt="Card image" style="border-radius: 2%;">
     <div class="card-img-overlay text-dark">
         <div class="d-flex align-items-end flex-column bd-highlight mb-3" style="height: 100%; width: 100%;">
             <div class="mt-auto p-2 bd-highlight w-100">
-                <div class="card">
+                    <h2 class="mb-5">{{ $user->name }}</h2>
+                <div class="card" style="border-radius: 1.25rem;">
                     <div class="card-body">
-                        <div class="row">
+                        {{-- <div class="row">
                             <div class="col-12 col-lg-8 col-md-6">
-                                {{-- <p class="lead">Web / UI Designer</p>
+                                <p class="lead">Web / UI Designer</p>
                                 <p>
                                     I love to read, hang out with friends, watch football, listen to music, and learn new things.
                                 </p>
                                 <p> <span class="badge badge-info tags">html5</span> 
                                     <span class="badge badge-info tags">css3</span>
                                     <span class="badge badge-info tags">nodejs</span>
-                                </p> --}}
+                                </p>
                             </div>
-                        </div>
+                        </div> --}}
                         <div class="row">
                             <div class="col-12 col-lg-4 text-center">
                                 <h3 class="mb-0">{{ $user->stories()->withTrashed()->select('coin_id')->where('user_id', $user->id)->groupBy('coin_id')->count() }}</h3>
