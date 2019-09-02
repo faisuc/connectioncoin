da72ef9a40e133f8fd5116b63a8f479cbdf6a419
Neil Carlo Sucuangco <succute@yahoo.com>
2019-09-03 03:32:35 +0800

    Update on profile 1. Change 'Coins' label to 'Coins Given' 2. Change 'Stories' to 'Connections Made'

--- resources/views/users/partials/sidebar.blade.php
+++ resources/views/users/partials/sidebar.blade.php
@@ -41,12 +41,12 @@
                         <div class="row">
                             <div class="col-12 col-lg-4 text-center">
                                 <h3 class="mb-0">{{ $user->stories()->withTrashed()->select('coin_id')->where('user_id', $user->id)->groupBy('coin_id')->count() }}</h3>
-                                <small>Coins</small>
+                                <small>Coins Given</small>
                                 <a href="{{ route('users.show', ['users' => $user]) }}" class="btn btn-block btn-outline-success mt-3" style="border-radius: 50px;"><i class="fas fa-user-circle"></i> My Profile</a>
                             </div>
                             <div class="col-12 col-lg-4 text-center">
                                 <h3 class="mb-0">{{ $user->stories()->withTrashed()->count() }}</h3>
-                                <small>Stories</small>
+                                <small>Connections Made</small>
                                 <a href="{{ route('users.coins.index', ['user' => $user]) }}" class="btn btn-block btn-outline-secondary mt-3" style="border-radius: 50px;"><i class="fas fa-coins"></i> Coins</a>
                             </div>
                             <div class="col-12 col-lg-4 text-center">
