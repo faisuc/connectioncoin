84077b1f383967e45ad3ab8be19525ea512a1c57
Neil Carlo Sucuangco <succute@yahoo.com>
2019-09-12 21:08:47 +0800

    Updates

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -5,7 +5,7 @@
     @method('PATCH')
     <div class="container-fluid py-4 container-profile100">
         <div class="row justify-content-center">
-            <div class="col-md-5 col-sm-5 col-lg-5">
+            <div class="col-md-12 col-sm-12 col-lg-5">
                 @include('users.partials.sidebar')
             </div>
         </div>
