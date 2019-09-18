dffc6888fed2727c1ca5f35650d17480a8c4bc3b
Neil Carlo Sucuangco <succute@yahoo.com>
2019-09-18 21:08:08 +0800

    Related stories updates

--- resources/views/stories/partials/_related-stories.blade.php
+++ resources/views/stories/partials/_related-stories.blade.php
@@ -1,16 +1,18 @@
-@foreach ($relatedStories as $story)
+@if (count($relatedStories))
     <h3 class="text-white">Related Stories</h3>
     <div class="row">
-        <div class="col-md-4">
-            <a href="{{ route('stories.show', ['story' => $story->id]) }}" class="custom-card">
-                <div class="card h-100 shadow">
-                    <img src="{{ $story->theImage }}" class="card-img-top" alt="{{ $story->title }}">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $story->title }}</h5>
-                        <p class="card-text">{{ $story->theDescription }}</p>
+        @foreach ($relatedStories as $story)
+            <div class="col-md-4">
+                <a href="{{ route('stories.show', ['story' => $story->id]) }}" class="custom-card">
+                    <div class="card h-100 shadow">
+                        <img src="{{ $story->theImage }}" class="card-img-top" alt="{{ $story->title }}">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $story->title }}</h5>
+                            <p class="card-text">{{ $story->theDescription }}</p>
+                        </div>
                     </div>
-                </div>
-            </a>
-        </div>
+                </a>
+            </div>
+        @endforeach
     </div>
-@endforeach
+@endif
